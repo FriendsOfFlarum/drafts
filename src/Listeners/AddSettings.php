dceabaf395f1c477725c12b61dc023652a52f7f4
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2020-05-25 14:46:46 -0400

    More StyleCI

--- src/Listeners/AddSettings.php
+++ src/Listeners/AddSettings.php
@@ -1,13 +1,13 @@
 <?php
 
 /*
-* This file is part of fof/drafts.
-*
-* Copyright (c) 2019 FriendsOfFlarum.
-*
-* For the full copyright and license information, please view the LICENSE.md
-* file that was distributed with this source code.
-*/
+ * This file is part of fof/drafts.
+ *
+ * Copyright (c) 2019 FriendsOfFlarum.
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
 
 namespace FoF\Drafts\Listeners;
 
