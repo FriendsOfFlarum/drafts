71b5617c91aff4946200577cf52866a89d138725
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2020-05-25 02:23:01 -0400

    Add schedule post permission

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -14,11 +14,23 @@ import { extend } from 'flarum/extend';
 import PermissionGrid from 'flarum/components/PermissionGrid';
 
 app.initializers.add('fof-drafts', app => {
+    extend(app, 'getRequiredPermissions', function (required, permission) {
+        if (permission === 'user.scheduleDrafts') {
+            required.push('user.saveDrafts');
+        }
+    });
+
     extend(PermissionGrid.prototype, 'startItems', items => {
         items.add('fof-draft-create', {
             icon: 'fa fa-edit',
             label: app.translator.trans('fof-drafts.admin.permissions.start'),
             permission: 'user.saveDrafts',
+        });
+
+        items.add('fof-draft-schedule', {
+            icon: 'fas fa-calendar-plus',
+            label: app.translator.trans('fof-drafts.admin.permissions.schedule'),
+            permission: 'user.scheduleDrafts',
         });
     });
 });
