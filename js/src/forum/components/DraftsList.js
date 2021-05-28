e4919c93c1f85ecd3b1cedba3e097f9b5bb754a9
David Wheatley <hi@davwheat.dev>
2021-05-28 23:36:30 +0100

    Fix delete all drafts button on mobile

--- js/src/forum/components/DraftsList.js
+++ js/src/forum/components/DraftsList.js
@@ -12,7 +12,7 @@
 import Component from 'flarum/common/Component';
 import LoadingIndicator from 'flarum/common/components/LoadingIndicator';
 import Button from 'flarum/common/components/Button';
-import DraftsListItem from "./DraftsListItem"
+import DraftsListItem from './DraftsListItem';
 import Tooltip from 'flarum/common/components/Tooltip';
 
 export default class DraftsList extends Component {
@@ -44,14 +44,16 @@ export default class DraftsList extends Component {
             <div className="NotificationList DraftsList">
                 <div className="NotificationList-header">
                     <h4 className="App-titleControl App-titleControl--text">{app.translator.trans('fof-drafts.forum.dropdown.title')}</h4>
-                    <Tooltip text={app.translator.trans("fof-drafts.forum.dropdown.delete_all_button")}>
-                      <Button
-                          data-container="body"
-                          icon="fas fa-trash-alt"
-                          className="Button Button--link Button--icon Alert-dismiss"
-                          onclick={this.deleteAll.bind(this)}
-                      />
-                    </Tooltip>
+                    <div class="App-primaryControl">
+                        <Tooltip text={app.translator.trans('fof-drafts.forum.dropdown.delete_all_button')}>
+                            <Button
+                                data-container="body"
+                                icon="fas fa-trash-alt"
+                                className="Button Button--link Button--icon Alert-dismiss"
+                                onclick={this.deleteAll.bind(this)}
+                            />
+                        </Tooltip>
+                    </div>
                 </div>
                 <div className="NotificationList-content">
                     <ul className="NotificationGroup-content">
@@ -59,15 +61,17 @@ export default class DraftsList extends Component {
                             ? drafts
                                   .sort((a, b) => b.updatedAt() - a.updatedAt())
                                   .map((draft) => {
-                                      return <DraftsListItem draft={draft} state={state} />
+                                      return <DraftsListItem draft={draft} state={state} />;
                                   })
                             : null}
 
-                        {state.loading
-                            ? <LoadingIndicator display="block" />
-                            : !drafts.length && (
-                                  <div className="NotificationList-empty">{app.translator.trans('fof-drafts.forum.dropdown.empty_text')}</div>
-                              )}
+                        {state.loading ? (
+                            <LoadingIndicator display="block" />
+                        ) : (
+                            !drafts.length && (
+                                <div className="NotificationList-empty">{app.translator.trans('fof-drafts.forum.dropdown.empty_text')}</div>
+                            )
+                        )}
                     </ul>
                 </div>
             </div>
