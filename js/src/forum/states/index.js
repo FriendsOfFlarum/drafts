708783b1cc0873fe3ee95cb47fe777a590204eaf
Rafael Horvat <rafael.horvat@glowingblue.com>
2021-09-09 14:56:11 +0200

    Added missing exports

---
+++ js/src/forum/states/index.js
@@ -0,0 +1,16 @@
+/*
+ *
+ *  This file is part of fof/drafts.
+ *
+ *  Copyright (c) 2021 FriendsOfFlarum.
+ *
+ *  For the full copyright and license information, please view the LICENSE.md
+ *  file that was distributed with this source code.
+ *
+ */
+
+import DraftsListState from './DraftsListState';
+
+export const models = {
+    DraftsListState: DraftsListState,
+};
