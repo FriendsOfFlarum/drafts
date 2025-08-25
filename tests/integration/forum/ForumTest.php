1155bffd52d4c8c0c1aeb41df9fdd3d52344bde1
Davide Iadeluca <davide.iadeluca@glowingblue.com>
2025-08-25 17:06:26 +0200

    test: setup basic backend testing

---
+++ tests/integration/forum/ForumTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace FoF\Drafts\Tests\integration\forum;
+
+use Flarum\Testing\integration\TestCase;
+
+class ForumTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->extension('fof-drafts');
+    }
+
+     /**
+     * @test
+     */
+    public function extension_boots_and_serializes()
+    {
+        $response = $this->send($this->request('GET', '/'));
+
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $body = (string) $response->getBody();
+
+        $this->assertStringStartsWith('<!doctype html>', $body);
+        $this->assertStringContainsString('</html>', $body);
+    }
+}
