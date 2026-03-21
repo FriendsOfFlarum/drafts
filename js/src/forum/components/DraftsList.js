61b1b36bbf8d0f2e3ccab3d5949667658f4871d4
Edge <dcc@monika.love>
2026-03-21 14:14:18 +0800

    sync

--- js/src/forum/components/DraftsList.js
+++ js/src/forum/components/DraftsList.js
@@ -25,7 +25,8 @@ export default class DraftsList extends Component {
     });
   }
 
-  deleteAll() {
+  deleteAll(e) {
+    $(e.currentTarget).trigger('mouseleave');
     if (!confirm(app.translator.trans('fof-drafts.forum.dropdown.delete_all_alert'))) return;
 
     app
